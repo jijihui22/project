6c35de597362727b1fc81f0c67b2ef27ca829061
jhkj333 <jhjh3383@naver.com>
2022-08-12 04:04:54 +0900

    3

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 let layer = document.querySelectorAll(".layer");
 
 window.addEventListener("scroll",function(){
-    console.log(window.pageYOffset)
+    console.log(window.pageYOffset);
+
     // 주어.style.transform = translateY()
     let scrollTop = window.pageYOffset;
-    
+   
+
     layer.forEach(function(item,index){
-        console.log(item.dataset.speed)    
+        console.log(item.dataset.speed); 
         let speed = item.dataset.speed;
-        item.style.transform = 'translate3d(${-scrollTop/speed}px,${scrollTop/speed*4}px,0)';
+      
+        item.style.transform = `translate3d(0,${scrollTop/speed*4}px,0)`; 
+    
     });
+
 });
