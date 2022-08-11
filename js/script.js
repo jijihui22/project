bc7d42be1e7ffa9be3553102c488a5162c029ce8
jhkj333 <jhjh3383@naver.com>
2022-08-12 05:32:16 +0900

    3

--- js/script.js
+++ js/script.js
@@ -1,18 +1,16 @@
-let layer = document.querySelectorAll(".layer");
-
 window.addEventListener("scroll",function(){
-    console.log(window.pageYOffset);
+    console.log(window.pageYOffset)
 
     // 주어.style.transform = translateY()
     let scrollTop = window.pageYOffset;
-   
-
-    layer.forEach(function(item,index){
-        console.log(item.dataset.speed); 
-        let speed = item.dataset.speed;
-      
-        item.style.transform = `translate3d(0,${scrollTop/speed*4}px,0)`; 
-    
-    });
+    document.querySelector(".pic0").style.transform = "translateY("+ scrollTop / 1 +"px)";
+    document.querySelector(".pic1").style.transform = "translateY("+ scrollTop / 3 +"px)";
+    document.querySelector(".pic2").style.transform = "translateY("+ scrollTop / 1 +"px)";
+    document.querySelector(".pic3").style.transform = "translateY("+ scrollTop / 6 +"px)";
+    document.querySelector(".pic4").style.transform = "translateY("+ scrollTop / 15 +"px)";
+    document.querySelector(".pic5").style.transform = "translateY("+ scrollTop / 16 +"px)";
+    document.querySelector(".pic6").style.transform = "translateY("+ scrollTop / 26 +"px)";
+    document.querySelector(".pic7").style.transform = "translateY("+ scrollTop / 60 +"px)";
+    document.querySelector(".pic8").style.transform = "translateY("+ scrollTop / 20 +"px)";
 
 });
